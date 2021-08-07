4907b3d3e90f81e33aac351fbbc93eaaa82c6422
thanhsang1999 <nguyenhuythanhsang@gmail.com>
2021-08-07 10:00:09 +0700

    add project 1

---
+++ Project1/script.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll(".panel");
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeClassActive();
+    panel.classList.add("active");
+  });
+});
+const removeClassActive = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
