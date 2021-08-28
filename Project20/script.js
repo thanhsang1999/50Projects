1c00d7b0dda0ce53a810c75364142f75adb348b1
thanhsang1999 <nguyenhuythanhsang@gmail.com>
2021-08-28 12:26:28 +0700

    project button ripple

---
+++ Project20/script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.ripple');
+Array.from(buttons).map((button) => {
+  button.addEventListener('click', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+    const circle = document.createElement('div');
+    circle.classList.add('circle');
+    circle.style.top = `${yInside}px`;
+    circle.style.left = `${xInside}px`;
+    button.appendChild(circle);
+    setTimeout(() => {
+      circle.remove();
+    }, 500);
+  });
+});
